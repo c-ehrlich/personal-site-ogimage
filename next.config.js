4149b568d589f5722270e1478af3acf5788de2ab
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-01-23 22:48:08 +0100

    no env

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@
 const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
 module.exports = (phase, { defaultConfig }) => {
-  try {
-    // validate env vars
-    require('./utils/env');
-  } catch (err) {
-    if (phase === PHASE_PRODUCTION_BUILD) {
-      // throw if prod build
-      throw err;
-    }
-  }
   /**
    * @type {import('next').NextConfig}
    */
