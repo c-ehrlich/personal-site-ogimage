5c253dbcacc44c242a4904429ed1686ed1bcf52f
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-01-27 18:05:45 +0100

    import all the inters

--- pages/api/blog.tsx
+++ pages/api/blog.tsx
@@ -8,10 +8,27 @@ export const config = {
 };
 
 export default async (req: Request) => {
-  const [inter900, inter700, inter400] = await Promise.all([
+  const [
+    inter900,
+    inter800,
+    inter700,
+    inter600,
+    inter500,
+    inter400,
+    inter300,
+    inter200,
+    inter100,
+  ] = await Promise.all([
+    // TODO: add back specific text
     fetchFont("Inter", 900),
+    fetchFont("Inter", 800),
     fetchFont("Inter", 700),
+    fetchFont("Inter", 600),
+    fetchFont("Inter", 500),
     fetchFont("Inter", 400),
+    fetchFont("Inter", 300),
+    fetchFont("Inter", 200),
+    fetchFont("Inter", 100),
   ]);
 
   const parsed = blogParams.decodeRequest(req);
@@ -23,7 +40,10 @@ export default async (req: Request) => {
 
   return new ImageResponse(
     (
-      <div tw="flex h-[630px] w-[1200px] flex-col overflow-clip bg-[hsl(15,35%,5%)] bg-cover p-16 text-[hsl(15,15%,90%)]">
+      <div
+        tw="flex h-[630px] w-[1200px] flex-col overflow-clip bg-[hsl(15,35%,5%)] bg-cover p-16 text-[hsl(15,15%,90%)]"
+        style={{ fontFamily: "Inter" }}
+      >
         <div tw="sticky right-10 top-0 z-0 h-4 w-4 rounded-full bg-transparent shadow-[1030px_600px_800px_800px_rgba(255,180,100,0.12)]" />
         <div tw="z-100 flex h-full w-full flex-col justify-between">
           <div tw="flex flex-col gap-16">
@@ -54,8 +74,14 @@ export default async (req: Request) => {
       height: 630,
       fonts: [
         { name: "Inter", data: inter900, weight: 900 },
+        { name: "Inter", data: inter800, weight: 800 },
         { name: "Inter", data: inter700, weight: 700 },
+        { name: "Inter", data: inter600, weight: 600 },
+        { name: "Inter", data: inter500, weight: 500 },
         { name: "Inter", data: inter400, weight: 400 },
+        { name: "Inter", data: inter300, weight: 300 },
+        { name: "Inter", data: inter200, weight: 200 },
+        { name: "Inter", data: inter100, weight: 100 },
       ],
     },
   );
--- pages/api/landing.tsx
+++ pages/api/landing.tsx
@@ -7,14 +7,35 @@ export const config = {
 };
 
 export default async (_req: Request) => {
-  const [inter800, inter700] = await Promise.all([
-    fetchFont("Inter", 800, "tRPC   Move Fast and Break Nothing"),
-    fetchFont("Inter", 700, "End-to-end typesafe APIs made easy. 0123456789,"),
+  const [
+    inter900,
+    inter800,
+    inter700,
+    inter600,
+    inter500,
+    inter400,
+    inter300,
+    inter200,
+    inter100,
+  ] = await Promise.all([
+    // TODO: add back specific text
+    fetchFont("Inter", 900),
+    fetchFont("Inter", 800),
+    fetchFont("Inter", 700),
+    fetchFont("Inter", 600),
+    fetchFont("Inter", 500),
+    fetchFont("Inter", 400),
+    fetchFont("Inter", 300),
+    fetchFont("Inter", 200),
+    fetchFont("Inter", 100),
   ]);
 
   return new ImageResponse(
     (
-      <div tw="flex h-[630px] w-[1200px] flex-col overflow-clip bg-[hsl(15,35%,5%)] bg-cover p-16 text-[hsl(15,15%,90%)]">
+      <div
+        tw="flex h-[630px] w-[1200px] flex-col overflow-clip bg-[hsl(15,35%,5%)] bg-cover p-16 text-[hsl(15,15%,90%)]"
+        style={{ fontFamily: "Inter" }}
+      >
         <div tw="sticky right-10 top-0 z-0 h-4 w-4 rounded-full bg-transparent shadow-[1030px_600px_800px_800px_rgba(255,180,100,0.12)]" />
         <div tw="z-100 flex h-full w-full flex-col items-center justify-center gap-16">
           <img
@@ -33,10 +54,17 @@ export default async (_req: Request) => {
         "Cache-Control": "s-maxage=86400, stale-while-revalidate",
       },
       width: 1200,
-      height: 600,
+      height: 630,
       fonts: [
+        { name: "Inter", data: inter900, weight: 900 },
         { name: "Inter", data: inter800, weight: 800 },
         { name: "Inter", data: inter700, weight: 700 },
+        { name: "Inter", data: inter600, weight: 600 },
+        { name: "Inter", data: inter500, weight: 500 },
+        { name: "Inter", data: inter400, weight: 400 },
+        { name: "Inter", data: inter300, weight: 300 },
+        { name: "Inter", data: inter200, weight: 200 },
+        { name: "Inter", data: inter100, weight: 100 },
       ],
     },
   );
