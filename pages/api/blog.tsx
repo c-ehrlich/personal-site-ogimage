8dbb84579727991d97c0751882b8679821de7ffb
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-01-27 22:19:24 +0100

    try

--- pages/api/blog.tsx
+++ pages/api/blog.tsx
@@ -72,7 +72,7 @@ export default async (req: Request) => {
               borderRadius: "50%",
               border: "1px solid green",
               backgroundColor: "red",
-              boxShadow: "5px 5px 50px 50px red",
+              boxShadow: "10px 10px 1px 1px red",
               // boxShadow: "1030px 600px 800px 800px rgba(255,140,100,0.12)",
             }}
           />
