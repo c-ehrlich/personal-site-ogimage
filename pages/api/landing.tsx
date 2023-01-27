181abd2356f3d7bef2e310864a9fcd575978d161
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2023-01-27 18:09:29 +0100

    test inline style

--- pages/api/landing.tsx
+++ pages/api/landing.tsx
@@ -34,7 +34,11 @@ export default async (_req: Request) => {
     (
       <div
         tw="flex h-[630px] w-[1200px] flex-col overflow-clip bg-[hsl(15,35%,5%)] bg-cover p-16 text-[hsl(15,15%,90%)]"
-        style={{ fontFamily: "Inter" }}
+        style={{
+          fontFamily: "Inter",
+          backgroundColor: "hsl(15,35%,5%)",
+          color: "hsl(15,15%,90%)",
+        }}
       >
         <div tw="sticky right-10 top-0 z-0 h-4 w-4 rounded-full bg-transparent shadow-[1030px_600px_800px_800px_rgba(255,180,100,0.12)]" />
         <div tw="z-100 flex h-full w-full flex-col items-center justify-center gap-16">
